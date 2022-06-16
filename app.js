6051162a6b687e03cb823f7daf8bb2123eb3b060
henry <enriqueafrutos@gmail.com>
2022-06-16 01:58:15 -0300

    agrego funcion

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-let peliculas = ['jurasic park', 'el padrino','forest gump','top gun', 'terminator', 'rapidos y furiosos'];
+let peliculas = ['jurasic park', ' el padrino' , ' forest gump','top gun', ' terminator', ' rapidos y furiosos '];
 let nombre = prompt('ingrese el nombre de la pelicula que quiere ver');
 if (peliculas.includes(nombre)){
     alert('la pelicula esta en cartelera');
 } else alert('no esta en cartelera')
 
-let agregarPeli = prompt('Dejanos que pelicula te gustaria agregar!')
-    peliculas.push(agregarPeli)
-    document.write('Peliculas disponibles ' + peliculas)
-
+    function agregar(nombre) {
+      peliculas.push(nombre)
+      return document.write('Peliculas disponibles: ' + peliculas)
+    }
+let consulta = prompt('te gustaria agregar una peli?')
+if(consulta == 'si'){
+agregar(prompt('dejanos que pelicula te gustaria agregar'))}
+else document.write('Peliculas disponibles: ' + peliculas)
